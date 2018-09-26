01fc5f8e76751bd0e167715de5b22dc6026b0b1e
Francesco Farina <rockerg991@gmail.com>
2018-09-26 23:04:47 +0100

    Timespan Support
    The goiptv module now supports the feature of searching in a time window.

--- goiptv.go
+++ goiptv.go
@@ -13,23 +13,65 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	lastHourQueryTemplate = "https://www.google.co.uk/search?q=%s+site:pastebin.com&source=lnt&tbs=qdr:h&sa=X&ved=0ahUKEwia3_bDrNndAhXKIMAKHTrIA2YQpwUIIw&biw=1440&bih=755"
+	lastDayQueryTemplate  = "https://www.google.co.uk/search?q=%s+site:pastebin.com&source=lnt&tbs=qdr:d&sa=X&ved=0ahUKEwjt-JSZrNndAhXJJMAKHXahDXIQpwUIIw&biw=1440&bih=755"
+	lastWeekQueryTemplate = "https://www.google.co.uk/search?q=%s+site:pastebin.com&source=lnt&tbs=qdr:w&sa=X&ved=0ahUKEwi4pteVrNndAhURQMAKHYZcDH0QpwUIIw&biw=1440&bih=755"
+	// LastHour is the argument to pass to the NewIPTVScraper function or to set as IPTVScraper.TimeSpan value,
+	// in order to let the scraper search the playlist generated in the last hour.
+	LastHour = "H"
+	// LastDay is the argument to pass to the NewIPTVScraper function or to set as IPTVScraper.TimeSpan value,
+	// in order to let the scraper search the playlist generated in the last 24 hours.
+	LastDay = "D"
+	// LastWeek is the argument to pass to the NewIPTVScraper function or to set as IPTVScraper.TimeSpan value,
+	// in order to let the scraper search the playlist generated in the last 7 days.
+	LastWeek = "W"
+)
+
+// IPTVScraper abstracts an IPTV Scraper.
+type IPTVScraper struct {
+	TimeSpan string
+}
+
+// NewIPTVScraper returns a new IPTV Scraper that will scrape iptv playlists
+// generated in the timespan passed as the only argument.
+// timeSpan: the only accepted values are
+func NewIPTVScraper(timeSpan string) *IPTVScraper {
+	return &IPTVScraper{
+		TimeSpan: timeSpan,
+	}
+}
+
 // ScrapeAll returns a channel of io.Readers from where reading the content of M3U playlist
-func ScrapeAll(tvchannels ...string) chan io.Reader {
+func (is *IPTVScraper) ScrapeAll(tvchannels ...string) chan io.Reader {
 	readerChan := make(chan io.Reader)
 	done := make(chan struct{})
 	for _, c := range tvchannels {
-		go scrape(c, readerChan, done)
+		go is.scrape(c, readerChan, done)
 	}
 	go waitAndClose(len(tvchannels), readerChan, done)
 	return readerChan
 }
 
-func scrape(tvchannel string, channel chan io.Reader, done chan struct{}) {
+func (is *IPTVScraper) scrape(tvchannel string, channel chan io.Reader, done chan struct{}) {
 	defer func() { done <- struct{}{} }()
-	var wg sync.WaitGroup
+	var (
+		wg      sync.WaitGroup
+		qFormat string
+	)
 
 	outData := make(chan *bytes.Buffer, 20)
-	qFormat := "https://www.google.co.uk/search?q=%s+site:pastebin.com&source=lnt&tbs=qdr:d&sa=X&ved=0ahUKEwipyeLr9o3dAhUIIMAKHfsmBdAQpwUIIA&biw=1308&bih=761"
+	switch is.TimeSpan {
+	case LastHour:
+		qFormat = lastHourQueryTemplate
+
+	case LastDay:
+		qFormat = lastDayQueryTemplate
+
+	case LastWeek:
+		qFormat = lastWeekQueryTemplate
+	}
+
 	link := fmt.Sprintf(qFormat, strings.Replace(tvchannel, " ", "+", -1))
 
 	c := colly.NewCollector()
